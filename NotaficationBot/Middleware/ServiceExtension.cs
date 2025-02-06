0ef6ea674206a9f0db051646ead41211cb80067d
Сергей Назаров <Sergej.Nazarov@vitacore.ru>
2025-02-06 15:33:40 +0300

    рефакторинг Programm.cs

---
+++ NotaficationBot/Middleware/ServiceExtension.cs
@@ -0,0 +1,108 @@
+﻿using NotificationsBot.Interfaces.Impl;
+using NotificationsBot.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using MinimalTelegramBot.StateMachine.Extensions;
+using MinimalTelegramBot.Builder;
+using MinimalTelegramBot;
+using NotificationsBot.States;
+using Telegram.Bot.Types.Enums;
+using MinimalTelegramBot.Handling;
+
+namespace NotificationsBot.Middleware;
+
+public static class ServiceExtension
+{
+    public static IServiceCollection AddImplInterfaces(this IServiceCollection services, IConfigurationManager configurationManager)
+    {
+
+        string connectionString = configurationManager.GetConnectionString("DefaultConnection");
+        services.AddDbContext<AppContext>(oprions =>
+        {
+            oprions.UseSqlite(connectionString);
+        });
+        services.AddScoped<IUsersDataService, UsersDataService>();
+        services.AddScoped<INotificationService, TelegramNotificationService>();
+
+        services.AddStateMachine();
+
+        return services;
+    }
+
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+
+    public static IHostApplicationBuilder ConfigureApplicationBuilder(this IHostApplicationBuilder hostApplicationBuilder)
+    {
+        hostApplicationBuilder.Services.AddImplInterfaces(hostApplicationBuilder.Configuration);
+
+        hostApplicationBuilder.Services.AddControllers()
+            .AddXmlSerializerFormatters();
+
+        hostApplicationBuilder.Services.AddSwagger();
+
+        return hostApplicationBuilder;
+    }
+
+    public static BotApplication ConfigureBotApplication(this BotApplication botApplication)
+    {
+        botApplication.UseStateMachine();
+        botApplication.HandleCommand("/start", () => "Hello, World!");
+        botApplication.HandleCommand("/register", async (BotRequestContext context) =>
+        {
+            var state = new RegisterState.AskLoginState();
+            await context.SetState(state);
+            return "Enter your login";
+        });
+        botApplication.HandleUpdateType(UpdateType.Message, async (string messageText, BotRequestContext context, IUsersDataService usersDataService) =>
+        {
+            try
+            {
+                var result = usersDataService?.SaveNewUser(messageText, context.ChatId);
+                if (result == null)
+                {
+                    throw new NullReferenceException("Регистрация клиента недоступна");
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                return ex.Message;
+            }
+            catch (Exception ex)
+            {
+                return "Наелся и спит -_-";
+            }
+            await context.DropState();
+            return "Все заебок";
+        }).FilterState<RegisterState.AskLoginState>();
+
+        return botApplication;
+    } 
+
+    public static WebApplication ConfigureWebApplication(this WebApplication webApplication)
+    {
+        if (webApplication.Environment.IsDevelopment())
+        {
+            webApplication.UseSwagger();
+            webApplication.UseSwaggerUI();
+        }
+
+        webApplication.UseHttpsRedirection();
+
+        webApplication.MapControllers();
+
+
+        return webApplication;
+    }
+
+    public static IBotApplicationBuilder ConfigureBotApplicationBuilder(this BotApplication botApplicationBuilder)
+    {
+        botApplicationBuilder.ConfigureBotApplication();
+        botApplicationBuilder.WebApplicationAccessor.ConfigureWebApplication();
+        return botApplicationBuilder;
+    }
+
+}
--- NotaficationBot/Program.cs
+++ NotaficationBot/Program.cs
@@ -1,15 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.TeamFoundation.SourceControl.WebApi;
-using Microsoft.VisualStudio.Services.Common;
-using Microsoft.VisualStudio.Services.WebApi;
-using MinimalTelegramBot;
-using MinimalTelegramBot.Builder;
-using MinimalTelegramBot.Handling;
-using MinimalTelegramBot.StateMachine.Extensions;
-using NotificationsBot.Interfaces;
-using NotificationsBot.Interfaces.Impl;
-using NotificationsBot.States;
-using Telegram.Bot.Types.Enums;
+﻿using MinimalTelegramBot.Builder;
+using NotificationsBot.Middleware;
 
 namespace NotificationsBot
 {
@@ -19,61 +9,13 @@ namespace NotificationsBot
         {
             //Uri uri = new Uri("https://tfs.dev.vitacore.ru/tfs");
             //VssConnection connection = new VssConnection(uri, new VssBasicCredential(string.Empty, "AzureDevopsToken"));
+            BotApplicationBuilder botBuilder = BotApplication.CreateBuilder(args);
 
-            var botBuilder = BotApplication.CreateBuilder(args);
-            string connectionString = botBuilder.Configuration.GetConnectionString("DefaultConnection");
-            botBuilder.Services.AddDbContext<AppContext>(oprions =>
-            {
-                oprions.UseSqlite(connectionString);
-            });
-            botBuilder.Services.AddScoped<IUsersDataService, UsersDataService>();
-            botBuilder.Services.AddScoped<INotificationService, TelegramNotificationService>();
-            botBuilder.Services.AddStateMachine();
-            botBuilder.Services.AddControllers()
-                .AddXmlSerializerFormatters();
-            botBuilder.Services.AddEndpointsApiExplorer();
-            botBuilder.Services.AddSwaggerGen();
+            botBuilder.ConfigureApplicationBuilder();
+
             BotApplication bothost = botBuilder.Build();
-            bothost.UseStateMachine();
-            bothost.HandleCommand("/start", () => "Hello, World!");
-            bothost.HandleCommand("/register", async (BotRequestContext context) =>
-            {
-                var state = new RegisterState.AskLoginState();
-                await context.SetState(state);
-                return "Enter your login";
-            });
-            bothost.HandleUpdateType(UpdateType.Message, async (string messageText, BotRequestContext context, IUsersDataService usersDataService) =>
-            {
-                try
-                {
-                    var result = usersDataService?.SaveNewUser(messageText, context.ChatId);
-                    if (result == null)
-                    {
-                        throw new NullReferenceException("Регистрация клиента недоступна");
-                    }
-                }
-                catch (NullReferenceException ex)
-                {
-                    return ex.Message;
-                }
-                catch(Exception ex)
-                {
-                    return "Наелся и спит -_-";
-                }
-                await context.DropState();
-                return "Все заебок";
-            }).FilterState<RegisterState.AskLoginState>();
 
-            if (bothost.WebApplicationAccessor.Environment.IsDevelopment())
-            {
-                bothost.WebApplicationAccessor.UseSwagger();
-                bothost.WebApplicationAccessor.UseSwaggerUI();
-            }
-
-            bothost.WebApplicationAccessor.UseHttpsRedirection();
-
-            bothost.WebApplicationAccessor.MapControllers();
-
+            bothost.ConfigureBotApplicationBuilder();
 
             bothost.Run();
         }
