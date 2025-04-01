dbfc96b9a821f0111525b0153812297cb682fcf8
Сергей Назаров <Sergej.Nazarov@vitacore.ru>
2025-04-01 11:10:34 +0300

    переимнована колонка

---
+++ NotificationBot/Migrations/20250401080827_RenameUserIdToChatId.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotificationsBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUserIdToChatId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NotificationsOnProjectChat_Users_UserId",
+                table: "NotificationsOnProjectChat");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "NotificationsOnProjectChat",
+                newName: "ChatId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NotificationsOnProjectChat_Users_ChatId",
+                table: "NotificationsOnProjectChat",
+                column: "ChatId",
+                principalTable: "Users",
+                principalColumn: "ChatId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NotificationsOnProjectChat_Users_ChatId",
+                table: "NotificationsOnProjectChat");
+
+            migrationBuilder.RenameColumn(
+                name: "ChatId",
+                table: "NotificationsOnProjectChat",
+                newName: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NotificationsOnProjectChat_Users_UserId",
+                table: "NotificationsOnProjectChat",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "ChatId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- NotificationBot/Models/Database/NotificationsOnProjectChat.cs
+++ NotificationBot/Models/Database/NotificationsOnProjectChat.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NotificationsBot.Models.Database
 {
     [PrimaryKey(nameof(UserId), nameof(ProjectId), nameof(NotificationTypesId))]
     public class NotificationsOnProjectChat
     {
+        [Column("ChatId")]
         public long UserId { get; set; }
         public User Users { get; set; }
         public int ProjectId { get; set; }
