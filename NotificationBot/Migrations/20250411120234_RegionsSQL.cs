ff57ef01ce1dbe24ae737fc8eaa957d19f8c93ba
Данил <116839456+felly991@users.noreply.github.com>
2025-04-14 16:41:05 +0300

    fix migrations (#83)
    Co-authored-by: Danila.Denisenko <Danila.Denisenko@DEV-013D>

--- NotificationBot/Migrations/20250411120234_RegionsSQL.cs
+++ NotificationBot/Migrations/20250411120234_RegionsSQL.cs
@@ -10,16 +10,16 @@ namespace NotificationsBot.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"insert into public.""Projects"" (""Name"") values ('AKUZ2');
+            migrationBuilder.Sql(@"insert into public.""Projects"" (""Name"") values ('AKUZ2');");
 
-insert into public.""NotificationTypes"" (""EventType"", ""EventDescription"") values
+            migrationBuilder.Sql(@"insert into public.""NotificationTypes"" (""EventType"", ""EventDescription"") values
 ('\\Regions\\NAO', 'NAO'),
 ('\\Regions\\Sevastopol', 'Sevastopol'),
 ('\\Regions\\Tatarstan', 'Tatarstan'),
 ('\\Regions\\Ulyanovsk', 'Ulyanovsk'),
-('\\', 'Others');
+('\\', 'Others');");
 
-insert into public.""NotificationTypesProjects"" (""ProjectsId"", ""NotificationTypesId"")
+            migrationBuilder.Sql(@"insert into public.""NotificationTypesProjects"" (""ProjectsId"", ""NotificationTypesId"")
 values
 ((select p.""Id"" from public.""Projects"" as p where p.""Name"" = 'AKUZ2'), (select nt.""Id"" from public.""NotificationTypes"" nt where nt.""EventType"" = '\\Regions\\NAO')),
 ((select p.""Id"" from public.""Projects"" as p where p.""Name"" = 'AKUZ2'), (select nt.""Id"" from public.""NotificationTypes"" nt where nt.""EventType"" = '\\Regions\\Sevastopol')),
