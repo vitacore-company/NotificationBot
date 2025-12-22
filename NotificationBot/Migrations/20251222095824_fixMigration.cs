b857173b4ca4d18cb9fb9db00c36c6ca6db62616
Sergey Nazarov <Sergej.Nazarov@vitacore.ru>
2025-12-22 14:45:28 +0300

    фикс миграции

--- NotificationBot/Migrations/20251222095824_fixMigration.cs
+++ NotificationBot/Migrations/20251222095824_fixMigration.cs
@@ -10,7 +10,7 @@ namespace NotificationsBot.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("ALTER DATABASE \"BotDb\" REFRESH COLLATION VERSION");
+            migrationBuilder.Sql(" DO $$\r\nDECLARE\r\n    db_name text;\r\nBEGIN\r\n    SELECT current_database() INTO db_name;\r\n    EXECUTE format('ALTER DATABASE %I REFRESH COLLATION version', db_name);\r\nEND $$;");
         }
 
         /// <inheritdoc />
