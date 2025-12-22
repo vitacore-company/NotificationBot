473b879edcf5c1172209f3076ea45776ee314fae
Sergey Nazarov <Sergej.Nazarov@vitacore.ru>
2025-12-22 13:16:14 +0300

    фикс версии postgresql

---
+++ NotificationBot/Migrations/20251222095824_fixMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotificationsBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER DATABASE \"BotDb\" REFRESH COLLATION VERSION");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
